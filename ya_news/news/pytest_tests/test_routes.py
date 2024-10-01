68c9c30b54805c452c543f528f9f9266340eafbb
Максим Юрченко <migismigis@gmail.com>
2024-10-01 21:49:58 +0700

    create author and another user edit delete comment tests.

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertRedirects
 
 '''
 В файле test_routes.py:
@@ -11,7 +12,7 @@ from django.urls import reverse
  комментария.
 При попытке перейти на страницу редактирования или удаления комментария
  анонимный пользователь перенаправляется на страницу авторизации.
-Авторизованный пользователь не может зайти на страницы редактирования 
++Авторизованный пользователь не может зайти на страницы редактирования 
 или удаления чужих комментариев (возвращается ошибка 404).
 +Страницы регистрации пользователей, входа в учётную запись и выхода из
  неё доступны анонимным пользователям.
@@ -42,15 +43,51 @@ def test_news_page_for_anonymous_user(client, news_object):
 
 
 @pytest.mark.parametrize(
-    'name, comment',
+    'user, name, comment, status_code',
     (
-            ('news:edit', pytest.lazy_fixture('comment_object')),
-            ('news:delete', pytest.lazy_fixture('comment_object')),
+            (pytest.lazy_fixture('author_client'),
+             'news:edit',
+             pytest.lazy_fixture('comment_object'),
+             HTTPStatus.OK,
+             ),
+
+            (pytest.lazy_fixture('author_client'),
+             'news:delete',
+             pytest.lazy_fixture('comment_object'),
+             HTTPStatus.OK,
+             ),
+
+            (pytest.lazy_fixture('not_author_client'),
+             'news:edit',
+             pytest.lazy_fixture('comment_object'),
+             HTTPStatus.NOT_FOUND,
+             ),
+
+            (pytest.lazy_fixture('not_author_client'),
+             'news:edit',
+             pytest.lazy_fixture('comment_object'),
+             HTTPStatus.NOT_FOUND,
+             ),
     ),
 )
 @pytest.mark.django_db
-def test_pages_availability_for_author(author_client, name, comment):
-    print(comment)
+def test_pages_availability_for_author(user, name, comment, status_code):
     url = reverse(name, args=(comment.pk,))
-    response = author_client.get(url)
-    assert response.status_code == HTTPStatus.OK
+    response = user.get(url)
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    'name, comment',
+    (
+            ('news:detail', pytest.lazy_fixture('comment_object')),
+            ('news:edit', pytest.lazy_fixture('comment_object')),
+    ),
+)
+@pytest.mark.django_db
+def test_redirects(client, name, comment):
+    login_url = reverse('users:login')
+    url = reverse(name, args=(comment.pk,))
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
