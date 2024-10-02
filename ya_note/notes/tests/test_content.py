dd5943f5f7e8f94c902b685c356fc7b279fbdffe
Максим Юрченко <migismigis@gmail.com>
2024-10-02 23:23:29 +0700

    unittest test routes and content finished

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,5 +1,61 @@
+from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+from ..models import Note
+from django.contrib.auth import get_user_model
+from ..forms import NoteForm
+
 '''
-отдельная заметка передаётся на страницу со списком заметок в списке object_list в словаре context;
-в список заметок одного пользователя не попадают заметки другого пользователя;
-на страницы создания и редактирования заметки передаются формы.
++отдельная заметка передаётся на страницу со списком заметок в
+ списке object_list в словаре context;
++в список заметок одного пользователя не попадают заметки
+ другого пользователя;
++на страницы создания и редактирования заметки передаются формы.
 '''
+User = get_user_model()
+
+
+class TestContent(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author1 = User.objects.create(username='Author1')
+        cls.author2 = User.objects.create(username='Author2')
+
+        cls.note1 = Note.objects.create(
+            title='Заголовок1',
+            text='Текст1',
+            slug='someslug1',
+            author=cls.author1,
+        )
+        cls.note2 = Note.objects.create(
+            title='Заголовок2',
+            text='Текст2',
+            slug='someslug2',
+            author=cls.author2,
+        )
+
+    def test_note_in_object_list(self):
+        self.client.force_login(self.author1)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertIn(self.note1, object_list)
+
+    def test_note_not_in_object_list(self):
+        self.client.force_login(self.author1)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertNotIn(self.note2, object_list)
+
+    def test_form_at_pages(self):
+        urls = (
+            ('notes:add', None),
+            ('notes:edit', (self.note1.slug,))
+        )
+        self.client.force_login(self.author1)
+        for url, args in urls:
+            response = self.client.get(reverse(url, args=args))
+            self.assertIn('form', response.context)
+            self.assertIsInstance(response.context['form'], NoteForm)
--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -6,23 +6,23 @@ from django.contrib.auth import get_user_model
 
 '''
 +Главная страница доступна анонимному пользователю.
-Аутентифицированному пользователю доступна
++Аутентифицированному пользователю доступна
     страница со списком заметок notes/,
     страница успешного добавления заметки done/,
     страница добавления новой заметки add/.
-Страницы
++Страницы
     отдельной заметки,
     удаления и
     редактирования заметки доступны 
     только автору заметки. 
     Если на эти страницы попытается зайти другой пользователь — вернётся ошибка 404.
-При попытке перейти на страницу списка заметок,
++При попытке перейти на страницу списка заметок,
     страницу успешного добавления записи,
     страницу добавления заметки,
     отдельной заметки,
     редактирования или удаления заметки
     анонимный пользователь перенаправляется на страницу логина.
-Страницы
++Страницы
     регистрации пользователей,
     входа в учётную запись
     и выхода из неё доступны всем пользователям.
@@ -38,22 +38,16 @@ class TestRoutes(TestCase):
         cls.author = User.objects.create(username='Author')
         cls.reader = User.objects.create(username='Reader')
 
-        cls.news = Note.objects.create(
+        cls.note = Note.objects.create(
             title='Заголовок',
             text='Текст',
-            slug='some_slug',
+            slug='someslug',
             author=cls.author,
         )
-
-    # def test_home_page(self):
-    #   url = reverse('notes:home')
-    #    response = self.client.get(url)
-    #    self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_pages_availability(self):
         urls = (
             ('notes:home', None),
-            ('notes:detail', (self.note.id,)),
             ('users:login', None),
             ('users:logout', None),
             ('users:signup', None),
@@ -63,3 +57,52 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_authentificated_user_pages_availability(self):
+        urls = (
+            ('notes:edit', (self.note.slug,)),
+            ('notes:detail', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+        )
+        users = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND)
+
+        )
+        for user, status in users:
+            self.client.force_login(user)
+            for name, args in urls:
+                with self.subTest(name=name):
+                    url = reverse(name, args=args)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_auth_user_pages(self):
+        urls = (
+            'notes:list',
+            'notes:success',
+            'notes:add'
+        )
+        self.client.force_login(self.reader)
+        for name in urls:
+            with self.subTest(name=name):
+                url = reverse(name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_anon_user_pages(self):
+        urls = (
+            ('notes:list', None),
+            ('notes:success', None),
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+            ('notes:detail', (self.note.slug,)),
+        )
+        login_url = reverse('users:login')
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
